f22e8cae7e41918a124b6563f08d8e5d0828e96c
xsl <jionsnxsl@163.com>
2018-04-17 18:00:29 +0800

    1.首页添加搜索框 2.添加搜索结果页面(没有样式)

--- applications/fishes/urls.py
+++ applications/fishes/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     # url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.homepage, name="homepage"),
+    url(r'^search-result/', views.search_view, name="search_view"),
 ]
--- applications/fishes/views.py
+++ applications/fishes/views.py
@@ -2,5 +2,13 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
+
 def homepage(request):
     return render(request, "homepage.html")
+
+
+def search_view(request):
+    fish_batch = request.GET.get("fish_batch")
+    print(fish_batch)
+    result = {"fish_batch": fish_batch}
+    return render(request, "search_result.html", result)
