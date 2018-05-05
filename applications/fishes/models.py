ed12907c26088480fe8a39dd8b72a9132782134c
xsl <jionsnxsl@163.com>
2018-05-05 16:48:49 +0800

    1.创建鱼池Model

--- applications/fishes/models.py
+++ applications/fishes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class FishPool(models.Model):
+    '''鱼池信息表'''
+    num = models.IntegerField(verbose_name='池子编号', unique=True)
+    radius = models.FloatField(verbose_name='池子半径')
+    depth = models.FloatField(verbose_name='池子深度')
+    PH = models.FloatField(verbose_name='PH值')
+    temperature = models.FloatField(verbose_name='温度')
+    fish_batch = models.IntegerField(verbose_name='鱼的批次', blank=True)
--- applications/fishes/urls.py
+++ applications/fishes/urls.py
@@ -7,12 +7,19 @@ urlpatterns = [
     url(r'^search-result/', views.search_view, name="search_view"),
     url(r'^admin/$', views.adminView, name="admin_view"),
     url(r'^admin/productinfo/$', views.productInfoView, name="productinfo"),
+
+    # 用户相关(START)
     url(r'^admin/userinfo/$', views.userInfoView, name="userinfo"),
     url(r'^admin/userdetail/(?P<uid>\d+)/$', views.userDetailView, name="userdetail"),
     url(r'^admin/adduser/$', views.AddUserView.as_view(), name="adduser"),
     url(r'^admin/deleteuser/$', views.deleteUserView, name="deleteuser"),
+    # 用户相关(END)
+
+    # 鱼池相关(START)
     url(r'^admin/fishpoolinfo/$', views.fishPoolInfoView, name="fishpoolinfo"),
     url(r'^admin/addfishpool/$', views.addFishPoolView, name="addfishpool"),
+    # 鱼池相关(END)
+
     url(r'^admin/generateercode/$', views.generateERCodeView, name="generateercode"),
     url(r'^admin/thirdtestreport/$', views.thirdPartTestReportView, name="thirdtestreport"),
     url(r'^admin/feedbacknotreply/$', views.feedbackNOTReplyView, name="feedbacknotreply"),
