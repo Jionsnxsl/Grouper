026b453b5faf5d937f1e4204bfb02a46541180e1
xsl <jionsnxsl@163.com>
2018-06-28 17:04:31 +0800

    提交一个BUG，登录拦截中间件中的访问路径没有获取访问参数；现在不进行修复，防止又出现其他问题。

--- utils/Middlewares.py
+++ utils/Middlewares.py
@@ -27,6 +27,7 @@ class LoginRequiredMiddleware(MiddlewareMixin):
                 work, ensure your TEMPLATE_CONTEXT_PROCESSORS setting includes\
                 'django.core.context_processors.auth'."
         if not request.user.is_authenticated():
+            # TODO : 这里发现一个BUG，request.path只获取了访问路径，没有获取访问参数，如果登录的时候是个GET请求就会出错
             path = request.path_info.lstrip('/')
             if not any(m.match(path) for m in EXEMPT_URLS):
                 return HttpResponseRedirect(settings.LOGIN_URL+"?next=/"+path)
